9d080fcae8cf95de89d059a74da9571479476410
freud.munera <freud.munera@DESARROLLO02>
2016-05-28 21:58:34 -0500

    Commit inicial

---
+++ VotacionesWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VotacionesWebSite.Startup))]
+namespace VotacionesWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
