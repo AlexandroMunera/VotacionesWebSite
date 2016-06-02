d3c260a327bb77064d285d10ca312b30d826c7e1
freud.munera <freud.munera@DESARROLLO02>
2016-06-02 18:38:10 -0500

    States Master - Index and Create states

---
+++ VotacionesWebSite/Controllers/StatesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VotacionesWebSite.Models;
+
+namespace VotacionesWebSite.Controllers
+{
+    public class StatesController : Controller
+    {
+        private VotacionesContext db = new VotacionesContext();
+
+        // GET: States
+        public ActionResult Index()
+        {
+            return View(db.States.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(State state)
+        {
+            if ( ! ModelState.IsValid)
+            {
+                return View(state);
+            }
+
+            db.States.Add(state);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+    }
+}
---
+++ VotacionesWebSite/Models/VotacionesContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace VotacionesWebSite.Models
+{
+    public class VotacionesContext : DbContext
+    {
+        public VotacionesContext()
+            : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<State> States { get; set; }
+
+    }
+}
